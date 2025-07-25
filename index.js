42e82777c4f5d27325dca4294bedc5bfef93c4d5
Tuscaney <tuddycarraway@gmail.com>
2025-07-25 18:09:16 -0500

    Create index.js
    Manually adding index.js file as I'm having computer issues.

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import functions from mathUtils.js
+import { add, multiply } from './mathUtils.js';
+
+// Import function from stringUtils.js
+import { capitalize } from './stringUtils.js';
+
+// Use the imported functions
+console.log('Add:', add(5, 3));             // Output: Add: 8
+console.log('Multiply:', multiply(4, 2));   // Output: Multiply: 8
+console.log('Capitalize:', capitalize('hello world')); // Output: Hello world
